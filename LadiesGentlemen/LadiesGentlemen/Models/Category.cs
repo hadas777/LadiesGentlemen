cc4c3f1ba6d75607f06128e06620ab5a69183e3d
dasin <dasinabul770@gmail.com>
2020-12-17 13:08:23 +0200

    dasi changes

---
+++ LadiesGentlemen/LadiesGentlemen/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LadiesGentlmen.Models
+{
+    public class Category
+    {
+       
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "You must enter a category name")]
+        [StringLength(100, ErrorMessage = "Hey, you can't enter more than 100 letters!")]
+        public string Name { get; set; }
+
+    }
+}
---
+++ LadiesGentlemen/LadiesGentlemen/Models/SubCart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LadiesGentlmen.Models
+{
+    public class SubCart
+    {
+        
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "You must enter quantity")]
+        [Range(0, 10, ErrorMessage = "Hey, you can order up to 10 items!")]
+        public int Quantity { get; set; }
+        public Stock Item { get; set; }
+
+    }
+}

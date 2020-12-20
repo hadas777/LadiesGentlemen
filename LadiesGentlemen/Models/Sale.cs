2c244c941a8846b875d5b273db0c79499675d397
hadasamano <hadas123498@gmail.com>
2020-12-20 19:04:28 +0200

    test

---
+++ LadiesGentlemen/Models/Sale.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LadiesGentlemen.Models
+{
+    public class Sale
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Percentage { get; set; }
+
+        public ICollection<ProductSale> Products { get; set; }
+    }
+}
